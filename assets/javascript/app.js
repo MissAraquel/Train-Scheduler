755699dbe16d6a86979865d24364a48c9e391176
Miss Araquel <missaraquel@gmail.com>
2018-07-20 22:18:28 -0700

    added script tags for firebase and initialized firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDOVMyL1mR3BAaR6MNFWghusRCTo3tMJGs",
+    authDomain: "train-scheduler-a4169.firebaseapp.com",
+    databaseURL: "https://train-scheduler-a4169.firebaseio.com",
+    projectId: "train-scheduler-a4169",
+    storageBucket: "train-scheduler-a4169.appspot.com",
+    messagingSenderId: "238965809359"
+  };
+  firebase.initializeApp(config);
